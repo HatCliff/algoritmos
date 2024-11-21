cdbf817f902b6e154cf6298c5308f51ced7291f0
pato0916 <patricio.pizarro32@gmail.com>
2024-11-21 11:51:27 -0300

    lectura de datos y transformacion de csv

--- EO/functions/procesamiento_datos.py
+++ EO/functions/procesamiento_datos.py
@@ -1,3 +1,4 @@
 import pandas as pd
 def leer_data(entrada):
     data = pd.read_csv(entrada, header=None)
+    return data 
--- EO/main.py
+++ EO/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
-
+#from functions.procesamiento_datos import * 
+import pandas as pd
 sep = os.path.sep 
 
 if len(sys.argv) == 6:
@@ -10,6 +11,31 @@ if len(sys.argv) == 6:
     entrada = "data"+ sep + sys.argv[4]
     salida = "data" + sep + sys.argv[5]
     print(semilla, tau, ite, entrada, salida)
+
+    data = pd.read_table(entrada,  header=None)
+    print(data)
+
+    nombre_problema=data[0][0]
+    n=int(data[0][1].split()[1])
+    c=int(data[0][2].split()[1])
+    z=int(data[0][3].split()[1])
+    print("nombre del problema: ", nombre_problema, type(nombre_problema))
+    print("n: ",n,type(n))
+    print("c: ",c,type(c))
+    print("z: ",z,type(z))
+
+    # Eliminar filas y procesar datos
+    data.drop(data.index[0:5], axis=0, inplace=True)  # Eliminar encabezados
+    data.drop(data.tail(1).index, axis=0, inplace=True)  # Eliminar última fila
+    print(data)
+    # Dividir columna en varias columnas
+    data = data[0].str.split(",", expand=True)
+    print(data)
+
 else:
-    print("Error en la entrada de datos, deben ser asi: Semilla(int), Tau(float),Iteración(int),Entrada_archivo(string),Salida_archivo(string)")
+    print("Error en la entrada de data, deben ser asi: Semilla(int), Tau(float),Iteración(int),Entrada_archivo(string),Salida_archivo(string)")
+
+
+
+
 
